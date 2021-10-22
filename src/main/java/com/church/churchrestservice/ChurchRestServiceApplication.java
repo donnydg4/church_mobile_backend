cc65ac750ca88aaea7991655a51d58d41131a98a
Donny Groezinger <donnygroezinger@gmail.com>
2021-10-21 21:02:13 -0500

    try something

--- src/main/java/com/church/churchrestservice/ChurchRestServiceApplication.java
+++ src/main/java/com/church/churchrestservice/ChurchRestServiceApplication.java
@@ -1,18 +1,31 @@
 package com.church.churchrestservice;
 
-import com.church.churchrestservice.repository.DisplayCardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class ChurchRestServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChurchRestServiceApplication.class, args);
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchRestServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/displayCards").allowedOrigins("http://localhost:8100");
+                registry.addMapping("/basePageCards").allowedOrigins("http://localhost:8100");
+                registry.addMapping("/seriesAndMinistries").allowedOrigins("http://localhost:8100");
+                registry.addMapping("/eventsAndActivities").allowedOrigins("http://localhost:8100");
+            }
+        };
+    }
+}
 
